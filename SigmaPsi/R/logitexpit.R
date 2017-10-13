e6c589a5f4a1c10232e40190806d2c88f8148322
livio <livio@stat.unipd.it>
2017-10-14 00:43:38 +0200

    pulizia

--- SigmaPsi/R/logitexpit.R
+++
@@ -1,8 +0,0 @@
-#'@title logit
-#'@aliases expit
-#'@param x value or vector of values
-#'@description logit and expit (its inverse) functions
-#'@export logit expit
-
-logit <- function(x) log(x/(1-x))
-expit <- function(x) exp(x)/(1+exp(x))
--- SigmaPsi/R/plotXresid.R
+++
@@ -1,9 +0,0 @@
-#'@title plotXresid
-#'@param mod lm model
-#'@param x name of the term to be ploted
-#'@description plot x vs residual of an lm model
-#'@export
-plotXresid <- function(mod,x){
-  plot(mod$model[,x],residuals(mod))
-  abline(0,0,col="red")
-}
